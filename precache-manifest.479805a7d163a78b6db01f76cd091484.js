dd79d29515bf4686c6248b3737c4a7bffaa50967
albinoplant <mateuszmalecki@hotmail.com>
2019-12-04 22:21:30 +0100

    Updates

---
+++ precache-manifest.479805a7d163a78b6db01f76cd091484.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "99042cd2ef17bea8835afbba1515e95f",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "ae2362d9b3d854c787f0",
+    "url": "/portfolio/static/css/main.2e159bb9.chunk.css"
+  },
+  {
+    "revision": "46bb1d3b8a80af3c5a26",
+    "url": "/portfolio/static/js/2.5ba3822b.chunk.js"
+  },
+  {
+    "revision": "ae2362d9b3d854c787f0",
+    "url": "/portfolio/static/js/main.cbc3592c.chunk.js"
+  },
+  {
+    "revision": "6bb3e531769fed2c8eb4",
+    "url": "/portfolio/static/js/runtime-main.7460d1ec.js"
+  },
+  {
+    "revision": "aa9768fd502ffd0ff9fda2c9d92cee70",
+    "url": "/portfolio/static/media/cv_eng.aa9768fd.pdf"
+  },
+  {
+    "revision": "6eef73a2bcec5e7b5d6b20a6e6966551",
+    "url": "/portfolio/static/media/page.6eef73a2.gif"
+  }
+]);
